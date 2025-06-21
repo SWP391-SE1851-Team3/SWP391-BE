c551f47312cd8e6880abba4d093ab99de628e11e
yynnxeko <121844439+yynnxeko@users.noreply.github.com>
2025-06-21 10:06:13 +0700

    Update MedicationSubmissionServiceInterface.java

--- src/main/java/com/team_3/School_Medical_Management_System/InterFaceSerivceInterFace/MedicationSubmissionServiceInterface.java
+++ src/main/java/com/team_3/School_Medical_Management_System/InterFaceSerivceInterFace/MedicationSubmissionServiceInterface.java
@@ -2,6 +2,7 @@ package com.team_3.School_Medical_Management_System.InterFaceSerivceInterFace;
 
 import com.team_3.School_Medical_Management_System.DTO.MedicationSubmissionDTO;
 import com.team_3.School_Medical_Management_System.DTO.MedicationSubmissionInfoDTO;
+import com.team_3.School_Medical_Management_System.DTO.MedicationDetailsExtendedDTO;
 import com.team_3.School_Medical_Management_System.Model.MedicationDetail;
 import com.team_3.School_Medical_Management_System.Model.MedicationSubmission;
 
@@ -10,15 +11,11 @@ import java.util.List;
 public interface MedicationSubmissionServiceInterface {
     MedicationSubmission submitMedication(MedicationSubmissionDTO medicationSubmissionDTO);
     List<MedicationSubmission> getAllMedicationSubmissionsByParentId(int parentId);
-    //    List<MedicationSubmission> getAllPendingMedicationSubmissions();
-//    List<MedicationSubmission> getAllSubmissionsByStatus(MedicationSubmission.SubmissionStatus submissionStatus);
-//    MedicationSubmission approveMedicationSubmission(int submissionId);
-//    MedicationSubmission rejectMedicationSubmission(int submissionId, String reason);
-//    MedicationSubmission confirmMedicationAdministered(int submissionId, String administrationNotes);
     List<MedicationSubmission> findAllSubmissions();
     public List<MedicationDetail> getDetailsBySubmissionId(int submissionId);
     void cancelMedicationSubmission(int submissionId);
     List<MedicationSubmissionInfoDTO> getAllMedicationSubmissionInfo();
     List<MedicationSubmissionInfoDTO> getMedicationSubmissionInfoByParentId(int parentId);
     MedicationSubmission getMedicationSubmissionById(int submissionId);
+    MedicationDetailsExtendedDTO getDetailsBySubmissionIdExtended(int submissionId);
 }
