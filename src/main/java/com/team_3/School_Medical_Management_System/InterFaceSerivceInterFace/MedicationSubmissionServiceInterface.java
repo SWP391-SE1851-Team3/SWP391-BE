49aa379a4e0a8a008674a405fd7d818d6f1d8e5f
Truong Hoang Long <longthse184073@fpt.edu.vn>
2025-06-21 10:22:17 +0700

    fix bug in github

--- src/main/java/com/team_3/School_Medical_Management_System/InterFaceSerivceInterFace/MedicationSubmissionServiceInterface.java
+++ src/main/java/com/team_3/School_Medical_Management_System/InterFaceSerivceInterFace/MedicationSubmissionServiceInterface.java
@@ -11,11 +11,6 @@ import java.util.List;
 public interface MedicationSubmissionServiceInterface {
     MedicationSubmission submitMedication(MedicationSubmissionDTO medicationSubmissionDTO);
     List<MedicationSubmission> getAllMedicationSubmissionsByParentId(int parentId);
-    //    List<MedicationSubmission> getAllPendingMedicationSubmissions();
-//    List<MedicationSubmission> getAllSubmissionsByStatus(MedicationSubmission.SubmissionStatus submissionStatus);
-//    MedicationSubmission approveMedicationSubmission(int submissionId);
-//    MedicationSubmission rejectMedicationSubmission(int submissionId, String reason);
-//    MedicationSubmission confirmMedicationAdministered(int submissionId, String administrationNotes);
     List<MedicationSubmission> findAllSubmissions();
     public List<MedicationDetail> getDetailsBySubmissionId(int submissionId);
     void cancelMedicationSubmission(int submissionId);
