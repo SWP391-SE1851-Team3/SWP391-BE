6def942992a6d1333ff11279c0fb8de3b4983c43
Hieu Tuan <tuannhse182788@fpt.edu.vn>
2025-07-23 11:08:42 +0700

    FIX getAll

--- src/main/java/com/team_3/School_Medical_Management_System/DTO/StudentMedicalEventDto.java
+++ src/main/java/com/team_3/School_Medical_Management_System/DTO/StudentMedicalEventDto.java
@@ -7,6 +7,8 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
+
 @Getter
 @Setter
 public class StudentMedicalEventDto {
@@ -14,7 +16,7 @@ public class StudentMedicalEventDto {
     private String studentNameAndClassName;
 
     @JsonProperty("eventType")// Học sinh (FullName + ClassName từ Student)
-    private String eventType;
+    private List<String> eventType;
     @JsonProperty("time")
     @JsonFormat(pattern = "HH:mm, dd/MM/yyyy")// Loại sự kiện (usageMethod từ MedicalEvent)
     private LocalDateTime time;
@@ -24,7 +26,7 @@ public class StudentMedicalEventDto {
     private String actions; // Hành động (chuỗi rỗng hoặc tùy chỉnh)
     private  Integer eventId;
     private Long eventDetailsID;
-    public StudentMedicalEventDto(String studentNameAndClassName, String eventType, LocalDateTime time, String processingStatus, String actions, Integer eventId,Long eventDetailsID) {
+    public StudentMedicalEventDto(String studentNameAndClassName, List<String> eventType, LocalDateTime time, String processingStatus, String actions, Integer eventId,Long eventDetailsID) {
         this.studentNameAndClassName = studentNameAndClassName;
         this.eventType = eventType;
         this.time = time;
